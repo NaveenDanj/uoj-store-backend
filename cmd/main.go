dc284b42922b13afe1cb9807f06e29268384c0ee
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-16 20:11:41 +0530

    deploy fixes

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"peer-store/db"
 	"peer-store/router"
 )
@@ -8,6 +10,15 @@ import (
 func main() {
 	db.Setup()
 	db.SeedAdminAccount()
+
+	go func() {
+		log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+		})))
+	}()
+
+	log.Fatal(http.ListenAndServeTLS(":443", "/etc/ssl/certs/selfsigned.crt", "/etc/ssl/private/selfsigned.key", nil))
+
 	r := router.SetupRouter()
 	r.Run(":5001")
 }
