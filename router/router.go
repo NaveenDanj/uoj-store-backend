85a818e968f5ad1f1d1d28c021e25ac0953ad440
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-17 14:28:10 +0530

    dep push 7

--- router/router.go
+++ router/router.go
@@ -12,7 +12,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "https://happy-island-02da9970f.5.azurestaticapps.net"},
+		AllowOrigins:     []string{"https://happy-island-02da9970f.5.azurestaticapps.net"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
