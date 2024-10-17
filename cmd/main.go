c3bf2142859df6bd44a69e9eef8fa9a87e841982
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-17 15:16:30 +0530

    dep push 9

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"log"
 	"peer-store/db"
 	"peer-store/router"
+	"net/http"
 )
 
 func main() {
 	db.Setup()
 	db.SeedAdminAccount()
 
-	// go func() {
-	// 	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 		http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
-	// 	})))
-	// }()
+	go func() {
+		log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+		})))
+	}()
 
 	r := router.SetupRouter()
 	r.Run(":5001")
 	// log.Fatal(http.ListenAndServeTLS(":443", "/etc/ssl/certs/selfsigned.crt", "/etc/ssl/private/selfsigned.key", r))
-
 }
