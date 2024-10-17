1f28f370b48be9cfa30eda21e77517cdb5371a4a
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-18 04:41:38 +0530

    added dep changes 13

--- router/router.go
+++ router/router.go
@@ -18,18 +18,6 @@ func SetupRouter() *gin.Engine {
 		AllowCredentials: true,
 	}))
 
-	r.OPTIONS("/*cors", func(c *gin.Context) {
-		c.AbortWithStatus(204)
-	})
-
-	r.OPTIONS("/api/*cors", func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://15.206.79.187")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.AbortWithStatus(204) // Return status 204 with no content for OPTIONS preflight
-	})
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
