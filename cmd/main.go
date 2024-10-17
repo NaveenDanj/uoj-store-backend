be80971777ec85d63f48538f94d4809f397348ff
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2024-10-17 20:03:32 +0530

    added dep changes 7

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"peer-store/db"
 	"peer-store/router"
 )
@@ -11,11 +9,11 @@ func main() {
 	db.Setup()
 	db.SeedAdminAccount()
 
-	go func() {
-		log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
-		})))
-	}()
+	// go func() {
+	// 	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+	// 	})))
+	// }()
 
 	r := router.SetupRouter()
 	r.Run(":5001")
